0d1e97f03aa7af1f4ea60aa985c322bddefbc005
ALFADHANI284 <alfadhanifahrezan@gmail.com>
2026-05-01 18:00:52 +0700

    Feat(AuthController): contoller untuk auth (login n register)

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
+class AuthController extends Controller
+{
+    // Register
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:6',
+            'role' => 'user'
+        ]);
+
+        if ($validator->fails()){
+            return response()->json($validator->errors(), 422);
+        }
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        $token = $user->createToken('FitTrack_AuthToken')->plainTextToken;
+
+        return response()->json([
+            'message' => 'Registrasi berhasil',
+            'data' => $user,
+            'access_token' => $token,
+            'token_type' => 'Bearer',
+            'role' => $role
+        ], 201);
+    }
+
+    // Login
+    public function login(Request $request){
+
+    if (!Auth::attempt($request->only('email', 'password'))) {
+        return response()->json([
+            'message' => 'Email atau password salah'
+        ], 401);
+    }
+
+    $user = User::where('email', $request->email)->firstOrFail();
+    $token = $user->createToken('FitTrack_AuthToken')->plainTextToken;
+
+    return response()->json([
+        'message' => 'Login berhasil',
+        'data' => $user,
+        'access_token' => $token,
+        'token_type' => 'Bearer'
+    ]);
+    }
+
+    //Logout 
+    public function logout(Request $request){
+        // Menghapus token yang sedang digunakan
+        $request->user()->currentAccessToken()->delete();
+
+        return response()->json([
+            'message' => 'Logout berhasil'
+        ]);
+    }
+}
