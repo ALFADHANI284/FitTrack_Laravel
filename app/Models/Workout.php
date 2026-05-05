55e76a13c4d0be75898fb4db4954e825c244f1c2
ALFADHANI284 <alfadhanifahrezan@gmail.com>
2026-05-05 19:29:23 +0700

    Feat(model): model workout, ubah yang tadi lagi

--- app/Models/Workout.php
+++ app/Models/Workout.php
@@ -4,8 +4,15 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-#[Fillable(['name', 'category_id', 'duration_minutes', 'calories_burned', 'description'])]
 class Workout extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'category_id', 
+        'name', 
+        'duration_minutes', 
+        'calories_burned', 
+        'description'
+    ];
 }
