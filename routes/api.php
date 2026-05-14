22a90c41b6d4b3270260843b9c5c363c620053ee
ALFADHANI284 <alfadhanifahrezan@gmail.com>
2026-05-14 22:11:58 +0700

    Feat(Routes): Api, tambah endpoint post untuk schedule / jadwal

--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Api\CategoryController;
 use App\Http\Controllers\Api\WorkoutController;
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\ProfileController;
+use App\Http\Controllers\Api\ScheduleController;
 use App\Http\Middleware\IsAdmin; 
 
 // Public Routes
@@ -14,17 +15,17 @@ Route::apiResource('/workouts', WorkoutController::class)->only(['index', 'show'
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
-    // Protected User Routes (harus ada Bearer Token di Header)
-    Route::middleware('auth:sanctum')->group(function (){
-        Route::post('/logout', [AuthController::class, 'logout']);
-        Route::get('/user', function (Request $request){  // Endpoint untuk ambil data user yang sedang login
-            return $request->user();
-        });
-        // Endpoint GET /api/profile
-        Route::get('/profile', [ProfileController::class, 'show']);
+// Protected User Routes (harus ada Bearer Token di Header)
+Route::middleware('auth:sanctum')->group(function (){
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::get('/user', function (Request $request){  // Endpoint untuk ambil data user yang sedang login
+        return $request->user();
     });
+    Route::get('/profile', [ProfileController::class, 'show']); // Endpoint GET /api/profile
+    Route::post('/schedules', [ScheduleController::class, 'store']); // Endpoint POST /api/schedules
+});
 
 // Protected Admin Routes (harus ada Bearer Token milik Admin)
 Route::middleware(['auth:sanctum', IsAdmin::class])->prefix('admin')->group(function () {
-    Route::apiResource('workouts', WorkoutController::class); // <--- Admin bebas akses semua (GET, POST, PUT, DELETE)
+    Route::apiResource('workouts', WorkoutController::class); // Admin bebas akses (GET, POST, PUT, DELETE)
 });
